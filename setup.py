6f91d97b10e1470f9ecb15d859de7643de10c47a
tija <alan@sonrai.co.uk>
2013-02-28 11:59:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-db-pool',
-    version='0.0.8',
+    version='0.0.85',
     author=u'Greg McGuire',
     author_email='gregjmcguire+github@gmail.com',
     packages=find_packages(),
